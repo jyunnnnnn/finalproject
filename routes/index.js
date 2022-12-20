387d476a904c1653f91246e2d9a02fb55a6e2118
jyunnnnnn <116063921+jyunnnnnn@users.noreply.github.com>
2022-12-20 19:23:15 +0800

    Add files via upload

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const MongoClient =require('mongodb').MongoClient; 
 const url= "mongodb+srv://admin:a12891289@cluster0.cd0rcje.mongodb.net/?retryWrites=true&w=majority";
-const chatrecord=[];
 router.get("/loadcaculus",function(req,res){
     chatrecord.length = 0;
     MongoClient.connect(url,function(err,db){
@@ -73,5 +72,32 @@ router.get("/loaddiscrete",function(req,res){
     });
   });
 });
-
+router.get("/loadca",function(req,res){
+  chatrecord.length = 0;
+  MongoClient.connect(url,function(err,db){
+    if(err)throw err;
+    console.log("連線成功load");
+    const dbo =db.db("mydb");
+    dbo.collection("CA").find({}).toArray(function (err, chatrecord) {
+      if (err) throw err;
+      res.send(chatrecord);
+      console.log(chatrecord);
+      db.close();
+    });
+  });
+});
+router.get("/loadds",function(req,res){
+  chatrecord.length = 0;
+  MongoClient.connect(url,function(err,db){
+    if(err)throw err;
+    console.log("連線成功load");
+    const dbo =db.db("mydb");
+    dbo.collection("ds").find({}).toArray(function (err, chatrecord) {
+      if (err) throw err;
+      res.send(chatrecord);
+      console.log(chatrecord);
+      db.close();
+    });
+  });
+});
 module.exports = router;
