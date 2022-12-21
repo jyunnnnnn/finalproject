bc66b5eb4c3d8e764bf6031c68b5c69e558c674f
jyunnnnnn <116063921+jyunnnnnn@users.noreply.github.com>
2022-12-22 01:17:35 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,68 @@ var express = require('express');
 var router = express.Router();
 const MongoClient =require('mongodb').MongoClient; 
 const url= "mongodb+srv://admin:a12891289@cluster0.cd0rcje.mongodb.net/?retryWrites=true&w=majority";
-router.post('/login',function(req,res){
-  const {username,password}=req.body;
+/*router.post('/login',function(req,res){
+  let {username,password}=req.body;
+  let a={username:username,password:password};
+  MongoClient.connect(url,function(err,db){
+    if(err)throw err;
+    console.log("連線成功load");
+    const dbo =db.db("mydb");
+    dbo.collection("account").find(a)(function (err, a) {
+      if (err){
+        dbo.collection("account").insertOne(a,function(err,res){
+          if (err) throw err;
+          console.log("1 document inserted");
+          db.close();
+        })
+      };
+      res.send(a);
+      console.log(a);
+      db.close();
+    });
+  });
     console.log("username=",username,"password=",password);
-    res.location("http://localhost:3000/main.html");
+    res.redirect("http://localhost:3000/main.html");
+});*/
+router.post("/signIn", function (req, res) {
+  //console.log(req.body);
+  console.log("Sign in now...");
+  const q = req.body;
+  MongoClient.connect(url,function(err,db){
+    if(err)throw err;
+    console.log("連線成功load");
+    const dbo =db.db("mydb");
+    dbo.collection("account").findOne({username:q.username},function (err, a) {
+      if (err) throw err;
+      if(a.password==q.password){
+        res.send("ok");
+      }
+      console.log("登入失敗");
+      res.send("fail");
+      db.close();
+    });
+  });
+});
+router.post("/signUp", function (req, res) {
+  //console.log(req.body);
+    console.log("Sign up now...");
+    const q = req.body;
+    MongoClient.connect(url,function(err,db){
+        if(err)throw err;
+        console.log("連線成功load");
+        const dbo =db.db("mydb");
+        dbo.collection("account").findOne({username:q.username},function(err,a){
+          if(err){
+            dbo.collection("account").insertOne({username:q.username,password:q.password},function (err, result) {
+              if (err) throw err;
+              res.send("ok");
+              console.log(a.username+" "+a.password);
+              db.close();
+            });
+          }
+          res.send("fail");
+        })
+    });
 });
 router.get("/loadcaculus",function(req,res){
     MongoClient.connect(url,function(err,db){
